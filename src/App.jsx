156fba9177c7cf57b0748f3c040f03826a857564
joelkwihangana <joelkwijoe@gmail.com>
2023-08-11 04:44:56 +0200

    updading the way to fetch data by using async await for easy code readability

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,22 @@ import "./App.css";
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  function fetchMoviesHandler() {
+
+  const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    fetch("https://swapi.dev/api/films/") // Note: Corrected URL
-      .then((response) => response.json())
-      .then((data) => {
-        setIsLoading(false);
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date, // Corrected property name
-          };
-        });
-        setMovies(transformedMovies);
-      });
-  }
+    const response = await fetch("https://swapi.dev/api/films/"); // Note: Corrected URL
+    const data = await response.json();
+    setIsLoading(false);
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date, // Corrected property name
+      };
+    });
+    setMovies(transformedMovies);
+  };
 
   return (
     <React.Fragment>
